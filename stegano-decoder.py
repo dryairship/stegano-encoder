6f8efa42c4eb67620190794e33a26833a62a1561
Priydarshi Singh <dryairship@gmail.com>
2018-11-27 03:16:15 +0530

    Added comments.

--- stegano-decoder.py
+++ stegano-decoder.py
@@ -2,26 +2,35 @@ import sys
 from PIL import Image
 
 def decode(encoded_file,output_file):
+	# image representation of the encoded file
 	encoded_image = Image.open(encoded_file)
+	# this array will store the pixels of the hidden file
 	pixels = []
 	
 	width, height = 0,0
+	# reading width from the first 5 pixels
 	for i in range(4,-1,-1):
 		r,g,b = encoded_image.getpixel((i%encoded_image.width, i//encoded_image.width))
 		width = (4*width)+r%4
 		width = (4*width)+g%4
 		width = (4*width)+b%4
+	# reading height from the next 5 pixels
 	for i in range(9,4,-1):
 		r,g,b = encoded_image.getpixel((i%encoded_image.width, i//encoded_image.width))
 		height = (4*height)+r%4
 		height = (4*height)+g%4
 		height = (4*height)+b%4
 
+    # image representation of the hidden file
 	output_image = Image.new('RGB',(width,height))
 
 	r,g,b=0,0,0
 	for i in range(10, 3*(height*width) + 10):
+		# l,m,n store the rgb values of a pixel in the encoded file
 		l,m,n = encoded_image.getpixel((i%encoded_image.width, i//encoded_image.width))
+		# the corresponding value (r/g/b) of the hidden image is read from 1 entire pixel (all 3 rgb values) of the encoded file
+		# %4 is used because only the last two bits of a color value store the information of the hidden file
+		# once we get 2 bits, we shift the bits to the left to accomodate new bits.
 		v = 0
 		v += n%4
 		v <<= 2
@@ -35,6 +44,7 @@ def decode(encoded_file,output_file):
 			g=v
 		else:
 			b=v
+			# we get one entire pixel of the hidden file after reading every 3 pixels of the encoded file
 			pixels.append((r,g,b))
 
 	output_image.putdata(pixels)
@@ -43,8 +53,10 @@ def decode(encoded_file,output_file):
 if __name__ == '__main__':
 	count = len(sys.argv)-1
 	if count == 1:
+	    # decoded file has not been specified. 'decoded.png' is used as default decoded file.
 		decode(sys.argv[1], 'decoded.png')
 	elif count == 2:
+	    # decoded file has been specified.
 		decode(sys.argv[1], sys.argv[2])
 	else:
 		print "Invalid arguments."
--- stegano-encoder.py
+++ stegano-encoder.py
@@ -2,11 +2,17 @@ import sys
 from PIL import Image
 
 def encode(visible_image,hidden_image,output_file):
+	# create an output image of the same size as the visible image
 	output_image = Image.new('RGB',(visible_image.width,visible_image.height))
+	# this array will store the rgb values of the encoded file
 	pixels = []
-	
+
 	w = hidden_image.width
 	h = hidden_image.height
+
+	# encoding width first
+	# the indexes (starting from right) in the binary representation of width are stored as :
+	# ... ( ~~~~~~(10)(11) ~~~~~~89 ~~~~~~67 ) ( ~~~~~~45 ~~~~~~23 ~~~~~~01 ) 
 	for i in range(0,5):
 		r,g,b = visible_image.getpixel((i%visible_image.width, i//visible_image.width))
 		r = ((r>>2)<<2) + (w>>4)%4
@@ -14,6 +20,8 @@ def encode(visible_image,hidden_image,output_file):
 		b = ((b>>2)<<2) + (w)%4
 		w = w>>6
 		pixels.append((r,g,b))
+
+	# height is encoded the same way as width
 	for i in range(5,10):
 		r,g,b = visible_image.getpixel((i%visible_image.width, i//visible_image.width))
 		r = ((r>>2)<<2) + (h>>4)%4
@@ -22,18 +30,23 @@ def encode(visible_image,hidden_image,output_file):
 		h = h>>6
 		pixels.append((r,g,b))
 
+	# c counts the number of pixels of hidden image encoded till now.
 	c = -1
 	for i in range(10, visible_image.height*visible_image.width):
 		if (i-1)%3==0:
 			c += 1
 		if(c<(hidden_image.width*hidden_image.height)):
+			# v stores the appropriate (R/G/B) color value depending on the value of i
 			v = hidden_image.getpixel((c%hidden_image.width, c//hidden_image.width))[(i-1)%3]
 			r,g,b = visible_image.getpixel((i%visible_image.width, i//visible_image.width))
 			r = ((r>>2)<<2) + (v>>2)%4
 			g = ((g>>2)<<2) + (v>>4)%4
 			b = ((b>>2)<<2) + (v>>6)%4
+			# the binary representation of a certain value of v is stored as :
+			# ( ~~~~~~32 ~~~~~~54 ~~~~~~76 )
 			pixels.append((r,g,b))
 		else:
+			# append the visible_image's pixels without modification.
 			pixels.append(visible_image.getpixel((i%visible_image.width, i//visible_image.width)))
 
 	output_image.putdata(pixels)
@@ -42,6 +55,9 @@ def encode(visible_image,hidden_image,output_file):
 def check(visible_file, hidden_file, output_file):
 	visible_image = Image.open(visible_file).convert('RGB')
 	hidden_image = Image.open(hidden_file).convert('RGB')
+	# the visible image should be 10 pixels larger than thrice the size of hidden image.
+	# thrice larger because 3 pixels of visible image store 1 pixel of hidden image
+	# 10 larger because size of the hidden image is stored in 10 initial pixels.
 	if visible_image.width*visible_image.height >= 3*(hidden_image.width*hidden_image.height) + 10:
 		encode(visible_image,hidden_image,output_file)
 	else:
@@ -51,9 +67,12 @@ def check(visible_file, hidden_file, output_file):
 if __name__ == '__main__':
 	count = len(sys.argv)-1
 	if count == 3:
+		# output file has been specified
 		check(sys.argv[1],sys.argv[2], sys.argv[3])
 	elif count == 2:
+		# output file has not been specified, 'output.png' is chosen as the default output file
 		check(sys.argv[1],sys.argv[2],'output.png')
 	else:
 		print "Invalid arguments."
 		print "Syntax : stegano-encoder.py visible_image.png hidden_image.png [output.png]"
+
